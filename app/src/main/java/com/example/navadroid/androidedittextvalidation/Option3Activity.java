6b1bd56377690aefd852f096ba3af229b340a257
APICHAT KHADSEETALEE <apichat_k@cmu.ac.th>
2017-12-12 01:12:58 +0700

    create Email validator using RegEx

--- app/src/main/java/com/example/navadroid/androidedittextvalidation/Option3Activity.java
+++ app/src/main/java/com/example/navadroid/androidedittextvalidation/Option3Activity.java
@@ -7,12 +7,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Option3Activity extends AppCompatActivity {
 
     private EditText etName;
     private EditText etPwd;
     private EditText etEmail;
     private EditText etPhone;
+
+    private static final String EMAIL_PATTERN = "^([A-Za-z0-9]+)([\\._A-Za-z0-9]+)(@)([A-Za-z0-9]+)(\\.[A-Za-z]{2,})$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +27,19 @@ public class Option3Activity extends AppCompatActivity {
         initView();
     }
 
-    private void bindView(){
+    private void bindView() {
         etName = (EditText) findViewById(R.id.et_name3);
         etPwd = (EditText) findViewById(R.id.et_pwd3);
         etEmail = (EditText) findViewById(R.id.et_email3);
         etPhone = (EditText) findViewById(R.id.et_phone3);
     }
 
-    private void initView(){
+    private void initView() {
         // OnClickListener
         findViewById(R.id.btn_validate3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(validateEditText()){
+                if (validateEditText()) {
                     Toast.makeText(Option3Activity.this, "Okay. You are good to go.", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -44,7 +49,7 @@ public class Option3Activity extends AppCompatActivity {
         etName.addTextChangedListener(new TextValidator(etName) {
             @Override
             public void validate(TextView textView, String text) {
-                if (etName.getText().toString().length() == 0) {
+                if (text.length() == 0) {
                     etName.setError("Required");
                 }
             }
@@ -53,23 +58,61 @@ public class Option3Activity extends AppCompatActivity {
         etPwd.addTextChangedListener(new TextValidator(etPwd) {
             @Override
             public void validate(TextView textView, String text) {
-                // TODO: add your Password validation here
+                if (text.length() == 0) {
+                    etPwd.setError("Required");
+                }
             }
         });
 
-        //etEmail.addTextChangedListener(...);
-        //etPhone.addTextChangedListener(...);
+        etEmail.addTextChangedListener(new TextValidator(etEmail) {
+            @Override
+            public void validate(TextView textView, String text) {
+                if (text.length() == 0) {
+                    etEmail.setError("Required");
+                }
+            }
+        });
+
+        etPhone.addTextChangedListener(new TextValidator(etPhone) {
+            @Override
+            public void validate(TextView textView, String text) {
+                if (text.length() == 0) {
+                    etPhone.setError("Required");
+                }
+            }
+        });
     }
 
 
     // To validate all EditTexts
-    private boolean validateEditText(){
+    private boolean validateEditText() {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(etEmail.getText().toString());
+
         boolean isValidated = true;
         if (etName.getText().toString().length() == 0) {
             etName.setError("Required");
             isValidated = false;
         }
-        // TODO: add your EditText validation here
+
+        if (etPwd.getText().toString().length() == 0) {
+            etPwd.setError("Required");
+            isValidated = false;
+        }
+
+        if (etEmail.getText().toString().length() == 0) {
+            etEmail.setError("Required");
+            isValidated = false;
+        }
+        else if (!matcher.find()) {
+            etEmail.setError("Invalid Email");
+            isValidated = false;
+        }
+
+        if (etPhone.getText().toString().length() == 0) {
+            etPhone.setError("Required");
+            isValidated = false;
+        }
 
         return isValidated;
     }
